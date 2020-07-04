5f67654694ba5eec77456ccfe6a35670889b11b0
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2020-07-04 08:29:38 -0300

    new version, revised requirements

--- setup.py
+++ setup.py
@@ -5,20 +5,26 @@ with open("README_PyPI.md", "r") as fh:
 
 setuptools.setup(
     name='glasspy',
-    version='0.3.dev1',
+    version='0.3',
     author='Daniel Roberto Cassar',
-    author_email='daniel.r.cassar@gmail.com',
+    author_email='contact@danielcassar.com.br',
     description='Python module for scientists working with glass materials',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/drcassar/glasspy",
     packages=setuptools.find_packages(),
     install_requires=[
-        'numpy>=1.18', 'scipy>=1.3', 'pandas>=1.0.0', 'lmfit>=1.0.0'
+        'numpy>=1.16',
+        'scipy>=1.2',
+        'pandas>=1.0.0',
+        'lmfit>=1.0.0',
+        'chemparse>=0.1.0',
     ],
     keywords='glass, non-crystalline materials',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
