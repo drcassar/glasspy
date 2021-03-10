0ba9ba112eb5e4aa44bbc5e5eb6c3295fd23b756
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2021-03-10 16:58:05 -0300

    update dependencies and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -21,13 +21,16 @@ setuptools.setup(
         'pandas>=1.0.0',
         'lmfit>=1.0.0',
         'chemparse>=0.1.0',
-        'mendeleev>=0.6.0'
+        'mendeleev>=0.6.0',
+        'torch>=1.7.0',
+        'pytorch-lightning',
     ],
     keywords='glass, non-crystalline materials',
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
