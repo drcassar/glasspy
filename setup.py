d9e2b35921dcd51ee2091a1ad7ddd91a9b9cf9e2
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2022-12-14 10:52:24 -0300

    update setup.py for 0.4 release

--- setup.py
+++ setup.py
@@ -2,43 +2,42 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read().replace(
-        '![Screenshot](doc/logo/logo_text_small.png)\n\n',
-        '')
+        "![Screenshot](doc/logo/logo_text_small.png)\n\n", ""
+    )
 
 setuptools.setup(
-    name='glasspy',
-    version='0.4.0.dev1',
-    author='Daniel Roberto Cassar',
-    author_email='contact@danielcassar.com.br',
-    description='Python module for scientists working with glass materials',
+    name="glasspy",
+    version="0.4.0",
+    author="Daniel Roberto Cassar",
+    author_email="daniel.r.cassar@gmail.com",
+    description="Python module for scientists working with glass materials",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/drcassar/glasspy",
     packages=setuptools.find_packages(),
     install_requires=[
-        'numpy>=1.16',
-        'scipy>=1.2',
-        'pandas>=1.0.0',
-        'lmfit>=1.0.0',
-        'chemparse>=0.1.0',
-        'mendeleev>=0.6.0',
-        'torch>=1.7.0',
-        'pytorch-lightning',
+        "numpy>=1.21",
+        "scipy>=1.8",
+        "pandas>=1.3",
+        "lmfit>=1.0.0",
+        "chemparse>=0.1.0",
+        # "mendeleev>=0.9.0",
+        # "torch>=1.7.0",
+        # "pytorch-lightning",
     ],
-    keywords='glass, non-crystalline materials',
+    keywords="glass, non-crystalline materials",
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3"
+            "or later (GPLv3+)",
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Science/Research",
         "Environment :: Console",
     ],
-    license='GPL',
-    python_requires='>=3.6',
+    license="GPL",
+    python_requires=">=3.9",
     include_package_data=True,
 )
