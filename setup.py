35e1932940014dc7b253ca83f0c6b42516953569
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2023-09-02 21:52:02 -0300

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="glasspy",
-    version="0.4.4",
+    version="0.4.5",
     author="Daniel Roberto Cassar",
     author_email="daniel.r.cassar@gmail.com",
     description="Python module for scientists working with glass materials",
